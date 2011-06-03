53c0273ce34b9772e58704fc9c54f1d22ee70985
mourner <agafonkin@gmail.com>
2011-06-04 01:28:28 +0300

    remove debug code
    Change-Id: I6f56c02794b44bbae449502a4930cf769f6de15a

--- src/kothic.js
+++ src/kothic.js
@@ -138,11 +138,11 @@ Kothic.render = function(canvasId, data, zoom, onRenderComplete, buffered) {
 			var box = [point[0] - w/2 - d, point[1] - h/2 - d, point[0] + w/2 - d, point[1] + h/2 - d, id];
 			this.buffer.push(box);
 			
-			ctx.save();
-			ctx.strokeStyle = 'red';
-			ctx.lineWidth = '1';
-			ctx.strokeRect(box[0], box[1], box[2] - box[0], box[3] - box[1]);
-			ctx.restore();
+//			ctx.save();
+//			ctx.strokeStyle = 'red';
+//			ctx.lineWidth = '1';
+//			ctx.strokeRect(box[0], box[1], box[2] - box[0], box[3] - box[1]);
+//			ctx.restore();
 		},
 
 		checkBox: function(b, id) {
