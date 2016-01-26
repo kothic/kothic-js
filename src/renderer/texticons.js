e0bb1905a23bf2649588d31d6f8dcefddc14e5c9
Rolf Eike Beer <eike@sf-mail.de>
2016-01-26 21:00:34 +0100

    add support for text-transform CSS property

--- src/renderer/texticons.js
+++ src/renderer/texticons.js
@@ -55,6 +55,13 @@ Kothic.texticons = {
                 textBaseline: 'middle'
             });
 
+            if (style['text-transform'] === 'uppercase')
+                text = text.toUpperCase();
+            else if (style['text-transform'] === 'lowercase')
+                text = text.toLowerCase();
+            else if (style['text-transform'] === 'capitalize')
+                text = text.replace(/(^|\s)\S/g, function(ch) { return ch.toUpperCase(); });
+
             if (feature.type === 'Polygon' || feature.type === 'Point') {
                 var textWidth = ctx.measureText(text).width,
                         letterWidth = textWidth / text.length,
